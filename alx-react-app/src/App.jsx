9ef4578a8ce4056ffcea38e289292d0bb15d60ce
mugisha-nar12 <mugishanarcisse1@gmail.com>
2025-08-26 17:59:32 +0200

    App.jsx: import components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,6 @@
-import Footer from './components/Footer'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
+import Footer from './components/Footer.jsx';
+import Header from './components/Header.jsx';
+import MainContent from './components/MainContent.jsx';
  
 
 const App = () => {
