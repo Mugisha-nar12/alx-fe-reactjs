9b2b51255616919fb3e73f4fb72036d1b6f41ab8
Mugisha narcisse <mugishanarcisse1@gmail.com>
2025-08-26 18:03:14 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,9 +6,9 @@ import MainContent from './components/MainContent.jsx';
 const App = () => {
   return (
     <div>
-      <Header/>
-      <Footer/>
-     <MainContent/>
+      <Header />
+      <MainContent />
+      <Footer />
       
     </div>
   )
