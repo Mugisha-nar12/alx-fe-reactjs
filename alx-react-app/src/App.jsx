4730c4858f788ee9660b0963e2cca7a731c3c85d
mugisha-nar12 <mugishanarcisse1@gmail.com>
2025-08-26 17:53:10 +0200

    src/components: add header, maincontent and footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,14 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
 import WelcomeMessage from './components/WelcomeMessage'  
 
 const App = () => {
   return (
     <div>
+      <Header/>
+      <MainContent/>
+      <Footer/>
       <WelcomeMessage />
     </div>
   )
