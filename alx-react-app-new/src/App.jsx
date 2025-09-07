c10ee3431613192ddf5e9e8b326e9c748ecfcf7c
mugisha-nar12 <mugishanarcisse1@gmail.com>
2025-09-07 08:56:22 +0200

    APP: add counter from component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,3 +1,4 @@
+import Counter from './components/Counter.jsx';
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
 import MainContent from './components/MainContent.jsx';
@@ -10,6 +11,7 @@ const App = () => {
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter/>
       <Footer />
       
     </div>
