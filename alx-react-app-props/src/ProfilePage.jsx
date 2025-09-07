3eccc9c51deb315b0b6fa7b274813cb369ae0456
mugisha-nar12 <mugishanarcisse1@gmail.com>
2025-09-07 09:46:31 +0200

    React application that uses prop drilling

---
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -0,0 +1,16 @@
+// remove userData props
+// import UserInfo from './UserInfo';
+
+// function ProfilePage({ userData }) {
+//   return <UserInfo userData={userData} />;
+// }
+
+// export default ProfilePage;
+
+import UserInfo from './UserInfo';
+
+function ProfilePage() {
+  return <UserInfo />;
+}
+
+export default ProfilePage;
---
+++ alx-react-app-props/src/UserInfo.jsx
@@ -0,0 +1,16 @@
+// remove userData props
+// import UserDetails from './UserDetails';
+
+// function UserInfo({ userData }) {
+//   return <UserDetails userData={userData} />;
+// }
+
+// export default UserInfo;
+
+import UserDetails from './UserDetails';
+
+function UserInfo() {
+  return <UserDetails />;
+}
+
+export default UserInfo;
