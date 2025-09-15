57c53468499d88ebc7b959efce0e7832cdcdca75
mugisha-nar12 <mugishanarcisse1@gmail.com>
2025-09-15 11:51:38 +0200

    App: improve it by adding routerprovider

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,9 @@
-import { Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
+import EditRecipeForm from './components/EditRecipeForm';
 import { useState } from 'react';
 
 function App() {
@@ -12,12 +14,21 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm addRecipe={addRecipe} />
-      <RecipeList recipes={recipes} />
-      <Outlet />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <AddRecipeForm addRecipe={addRecipe} />
+              <RecipeList recipes={recipes} />
+            </>
+          } />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/edit-recipe/:id" element={<EditRecipeForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
