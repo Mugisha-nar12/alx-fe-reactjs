1a78a07daabcfe9175e31a9b943dc5a8a812a5bf
mugisha-nar12 <mugishanarcisse1@gmail.com>
2025-09-20 19:18:35 +0200

    import vercel

--- github-user-search/src/main.jsx
+++ github-user-search/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import router from './router';
 import { ThemeProvider } from './context/ThemeContext';
 import './index.css';
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <RouterProvider router={router} future={{ v7_startTransition: true }} />
+        <SpeedInsights />
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>
