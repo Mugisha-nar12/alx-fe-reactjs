0206a645627a8c6763b4ffa23ab53c4c4ce675fc
mugisha-nar12 <mugishanarcisse1@gmail.com>
2025-10-05 20:48:11 +0200

    update homepage

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { RecipeCard } from "./RecipeCard";
 // eslint-disable-next-line no-unused-vars
 import recipesData from "../data.json";
 
 // Styling for hover, rounded, and shadow effects are in RecipeCard.jsx
-// Navigation logic is handled in RecipeCard.jsx
 
 const HomePage = ({ recipes: recipesFromProps }) => {
   const [recipes, setRecipes] = useState(recipesFromProps);
@@ -21,7 +21,9 @@ const HomePage = ({ recipes: recipesFromProps }) => {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {recipes.length > 0 ? (
           recipes.map((recipe) => (
-            <RecipeCard key={recipe.id} recipe={recipe} />
+            <Link to={`/recipe/${recipe.id}`} key={recipe.id} className="block">
+              <RecipeCard recipe={recipe} />
+            </Link>
           ))
         ) : (
           <p className="col-span-full text-center text-gray-600 text-lg">
